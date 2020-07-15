e341792eb3b2a4211b507d8daea23b2ab7eb6ca9
craigshoemaker <craig@craigshoemaker.net>
2020-07-15 15:41:41 -0400

    Implement table storage persistence

--- functions/add/index.js
+++ functions/add/index.js
@@ -3,7 +3,7 @@ const params = require("../modules/params");
 
 module.exports = async function (context, req) {
   context.bindings.tableBinding = [];
-  const url = params.get(req, url);
+  const url = params.get(req, "url");
   const resource = createResource(url);
   context.bindings.tableBinding.push(resource);
   context.done();
