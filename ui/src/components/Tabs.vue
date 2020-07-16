921a737b88974df813022ed58fa0a30f0eb4cbc0
craigshoemaker <craig@craigshoemaker.net>
2020-07-16 08:08:55 -0400

    Update Tabs

--- ui/src/components/Tabs.vue
+++ ui/src/components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <ul>
-      <li>Getting Started</li>
+  <div class="container mx-auto">
+    <ul class="tabs">
+      <li class="selected">Getting Started</li>
       <li>Tools</li>
       <li>VS Code Extensions</li>
     </ul>
