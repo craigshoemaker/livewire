6f1d681850f3c2523b1750e0e94d374900247ccd
craigshoemaker <craig@craigshoemaker.net>
2020-07-16 08:08:25 -0400

    Update header

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
-  <header>
-    Microsoft | Livewire
+  <header class="border-b-1 sm:p-0 sm:pb-3">
+    <div class="container mx-auto">
+      <img src="images/microsoft-logo.png" alt="Microsoft" class="inline" />
+      <span class="text-gray-600 text-2xl ml-3 mr-3">|</span>
+      <img src="images/livewire-logo.png" alt="Livewire" class="inline" />
+    </div>
   </header>
 </template>
 
