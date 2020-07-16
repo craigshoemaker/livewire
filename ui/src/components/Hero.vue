3e9f2b61fc6728e5fcb6f388e3ad68e73db73508
craigshoemaker <craig@craigshoemaker.net>
2020-07-16 08:09:09 -0400

    Update Hero component

--- ui/src/components/Hero.vue
+++ ui/src/components/Hero.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1 class="text-4xl">{{ title }}</h1>
+  <div class="container mx-auto mt-8 mb-8">
+    <h1 class="text-4xl font-semibold">{{ title }}</h1>
+    <p>{{ msg }}</p>
   </div>
 </template>
 
@@ -9,6 +10,7 @@ export default {
   name: "Hero",
   props: {
     title: String,
+    msg: String,
   },
 };
 </script>
