8547637af784210b3da57e11d9e8328a2d2833e9
craigshoemaker <craig@craigshoemaker.net>
2020-07-20 15:41:50 -0400

    Update add - custom/detailed response messages - removed table storage bindings - call data service explicitly - only attempts to save URL if it doesn't already exist

--- api/add/core.js
+++ api/add/core.js
@@ -1,11 +1,15 @@
+const { get, add } = require("../modules/resourcesData");
+
 const _module = {
   GITHUB_PATTERN: /https?\:\/\/github\.com\//,
   VSCODE_MARKETPLACE_PATTERN: /https:\/\/marketplace\.visualstudio\.com\/items\?itemName\=/,
+
   getGitHubKeyByUrl: (gitHubUrl) => {
     let key = gitHubUrl.replace(_module.GITHUB_PATTERN, "");
     key = key.replace("/", "-");
     return key;
   },
+
   getExtensionKeyByUrl: (vsCodeExtensionUrl) => {
     let key = vsCodeExtensionUrl.replace(
       _module.VSCODE_MARKETPLACE_PATTERN,
@@ -13,11 +17,13 @@ const _module = {
     );
     return key;
   },
+
   getKeyByUrl: (url) => {
     return _module.isGitHubUrl(url)
       ? _module.getGitHubKeyByUrl(url)
       : _module.getExtensionKeyByUrl(url);
   },
+
   createResource: (url) => {
     const isGitHub = _module.isGitHubUrl(url);
     const partitionKey = isGitHub ? "repository" : "extension";
@@ -28,8 +34,63 @@ const _module = {
       url: url,
     };
   },
+
   isGitHubUrl: (url) => url.match(_module.GITHUB_PATTERN),
+
   isExtensionUrl: (url) => url.match(_module.VSCODE_MARKETPLACE_PATTERN),
+
+  addUrlIfDoesNotExist: async (url) => {
+    return new Promise(async (resolve, reject) => {
+      const responses = {
+        added: {
+          status: 200,
+          body: "Added",
+        },
+        error: {
+          status: 500,
+          body: "An error occurred while trying to to add the resource.",
+        },
+        exists: {
+          status: 400,
+          body: "URL already exists.",
+        },
+        invalidUrl: {
+          status: 400,
+          body:
+            "A GitHub repository or Visual Studio Code extension marketplace URL is required.",
+        },
+      };
+
+      let response = {};
+
+      try {
+        const { createResource, isExtensionUrl, isGitHubUrl } = _module;
+
+        if (isGitHubUrl(url) || isExtensionUrl(url)) {
+          const resource = createResource(url);
+          const getResponse = await get(resource.PartitionKey, resource.RowKey);
+          const exists = !!getResponse.Timestamp;
+
+          if (!exists) {
+            const addResponse = await add(resource);
+            if (addResponse[".metadata"].etag) {
+              response = responses.added;
+            } else {
+              response = responses.error;
+            }
+          } else {
+            response = responses.exists;
+          }
+        } else {
+          response = responses.invalidUrl;
+        }
+
+        resolve(response);
+      } catch (exception) {
+        reject(exception);
+      }
+    });
+  },
 };
 
 module.exports = _module;
--- api/add/index.js
+++ api/add/index.js
@@ -1,18 +1,9 @@
-const { createResource, isGitHubUrl, isExtensionUrl } = require("./core");
+const { addUrlIfDoesNotExist } = require("./core");
 const params = require("../modules/params");
 
 module.exports = async function (context, req) {
   const url = params.get(req, "url");
-  if (isGitHubUrl(url) || isExtensionUrl(url)) {
-    const resource = createResource(url);
-    context.bindings.tableBinding = [];
-    context.bindings.tableBinding.push(resource);
-    context.done();
-  } else {
-    context.res = {
-      status: 400,
-      body:
-        "A GitHub repository or Visual Studio Code extension marketplace URL is required.",
-    };
-  }
+  const response = await addUrlIfDoesNotExist(url);
+  context.res = response;
+  context.done();
 };
