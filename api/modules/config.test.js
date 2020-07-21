d314233501a65262024e605dc7ffb5ae678f7e28
craigshoemaker <craig@craigshoemaker.net>
2020-07-21 14:25:52 -0400

    Fix regex evaluation

--- api/modules/config.test.js
+++ api/modules/config.test.js
@@ -1,8 +1,8 @@
 const config = require("./config");
 
 test("config should read from local.settings.json when not running in production.", async () => {
-  const isMatch = config.STORAGE_CONNECTION_STRING.match(
-    /DefaultEndpointsProtocol=https;AccountName.*?EndpointSuffix=core\.windows\.net/
+  const isMatch = /DefaultEndpointsProtocol=https;AccountName.*?EndpointSuffix=core\.windows\.net/.test(
+    config.STORAGE_CONNECTION_STRING
   );
   expect(isMatch).toBeTruthy();
 });
