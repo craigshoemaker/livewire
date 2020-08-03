07ee06655ee7b9df30c256c14d70d4e805e0e62c
craigshoemaker <craig@craigshoemaker.net>
2020-08-03 13:22:32 -0400

    Update config

--- api/modules/config.test.js
+++ api/modules/config.test.js
@@ -1,8 +1,18 @@
 const config = require("./config");
 
-test("config should read from local.settings.json when not running in production.", async () => {
+test("config should read Connection String from local.settings.json when not running in production.", async () => {
   const isMatch = /DefaultEndpointsProtocol=https;AccountName.*?EndpointSuffix=core\.windows\.net/.test(
     config.STORAGE_CONNECTION_STRING
   );
+  expect(isMatch).toBeTruthy();
+});
+
+test("config should read Account Name from local.settings.json when not running in production.", async () => {
+  const isMatch = /livewireapp/.test(config.STORAGE_ACCOUNT_NAME);
+  expect(isMatch).toBeTruthy();
+});
+
+test("config should read Account Key from local.settings.json when not running in production.", async () => {
+  const isMatch = config.STORAGE_ACCOUNT_KEY !== "";
   expect(isMatch).toBeTruthy();
 });
