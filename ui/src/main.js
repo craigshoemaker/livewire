34602e7951d3f4463912058357045f8605364d30
Cephas Lin <cephalin@microsoft.com>
2020-09-01 16:52:36 +0200

    add routing #14

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,10 +1,27 @@
 import Vue from "vue";
+import VueRouter from 'vue-router'
 import App from "./App.vue";
 
 import "./assets/app.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter)
+
+const Foo = { template: '<div></div>' }
+
+const router = new VueRouter({
+  mode: 'history', // use hashless route
+  routes: [
+    { path: '/getting-started', name: "getting-started", component: Foo},
+    { path: '/tools', name: "tools", component: Foo},
+    { path: '/extensions', name: "extensions", component: Foo},
+    { path: '/', redirect: '/getting-started'},
+    { path: "*", redirect: '/getting-started'}
+  ]
+})
+
 new Vue({
+  router: router,
   render: (h) => h(App),
 }).$mount("#app");
