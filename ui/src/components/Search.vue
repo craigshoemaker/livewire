953a0363c6a8dfc731d583c8b42b842ea9c728dc
craigshoemaker <craig@craigshoemaker.net>
2020-10-01 09:02:57 -0400

    Make search box 100% width

--- ui/src/components/Search.vue
+++ ui/src/components/Search.vue
@@ -2,7 +2,7 @@
   <div class="px-6 pb-4">
     <input
       type="text"
-      class="text-lg p-2 shadow-inner border rounded-md mr-2"
+      class="text-lg p-2 shadow-inner border rounded-md mr-2 w-full"
       placeholder="Search"
       v-model="searchText"
       @input="emitSearchText()"
