cf855c805a5046f5c0234a0170726fc408b21ab3
Mangesh Sangapu <mangesh@microsoft.com>
2020-10-02 14:42:38 -0500

    App.css and Classes update

--- ui/src/components/ExtensionCard.vue
+++ ui/src/components/ExtensionCard.vue
@@ -2,10 +2,10 @@
   <div class="card">
     <div class="tags px-6 py-4">
       <div>
-        <span class="font-semibold"><a :href="resource.url" target="_blank" rel="noopener noreferrer">{{resource.title}}</a></span>
+        <span class="title"><a :href="resource.url" target="_blank" rel="noopener noreferrer">{{resource.title}}</a></span>
       </div>
       <div>
-        <span class="extension-tag">{{ resource.description }}</span>
+        <p>{{ resource.description }}</p>
       </div>
     </div>
   </div>
