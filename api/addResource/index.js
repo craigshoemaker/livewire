71ab9cb76766c4b42f9741596d2ac37e9bcea838
craigshoemaker <craig@craigshoemaker.net>
2020-10-16 14:49:07 -0400

    Fix addResource function - remove branch - update import statement

--- api/addResource/index.js
+++ api/addResource/index.js
@@ -1,10 +1,10 @@
-const { run } = require("./add");
+const { run } = require("./addResource");
 const { get } = require("../modules/utils/params");
 
 module.exports = async function (context, req) {
   try {
     const $ = (key) => get(req, key);
-    context.res = await run($("url"), $("branch"));
+    context.res = await run($("url"));
   } catch (ex) {
     context.res = ex;
   }
