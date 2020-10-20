01f64cdb51ccba6c318291ff7bd53084ae873b29
craigshoemaker <craig@craigshoemaker.net>
2020-10-20 14:28:10 -0400

    ResourceCards with horizontal stacking

--- ui/src/components/ResourceCard.vue
+++ ui/src/components/ResourceCard.vue
@@ -1,53 +1,63 @@
 <template>
   <div class="card p-6">
-    <div>
-      <div class="title">
-        <a :href="resource.url" target="_blank">{{ resource.title }}</a>
+    <div
+      v-if="!/repository|extension/.test(resource.PartitionKey)"
+      class="mb-12"
+    >
+      <h3 class="text-2xl">Unknown Data Type</h3>
+      <p>Unknown partition key value: {{ resource.PartitionKey }}</p>
+    </div>
+
+    <div v-else>
+      <div>
+        <div class="title">
+          <a :href="resource.url" target="_blank">{{ resource.title }}</a>
+        </div>
+        <p class="text-gray-700 text-base">
+          {{ resource.description }}
+        </p>
       </div>
-      <p class="text-gray-700 text-base">
-        {{ resource.description }}
-      </p>
-    </div>
 
-    <div v-if="resource.videoURL" class="mt-4">
-      <a :href="resource.videoURL" target="_blank">Demo video</a>
-    </div>
+      <div v-if="resource.videoURL" class="mt-4">
+        <a :href="resource.videoURL" target="_blank">Demo video</a>
+      </div>
 
-    <div v-if="/repository/.test(resource.PartitionKey)">
-      <div class="mt-4">
-        <div class="flex">
-          <div class="facet-label">Categories</div>
-          <div class="facet-list">
-            <span
-              class="resource-tag"
-              v-for="category in resource.categories"
-              :key="category"
-              >{{ category }}</span
-            >
+      <div v-if="/repository/.test(resource.PartitionKey)">
+        <div class="mt-4">
+          <div class="flex">
+            <div class="facet-label">Categories</div>
+            <div class="facet-list">
+              <span
+                class="resource-tag"
+                v-for="category in resource.categories"
+                :key="category"
+                >{{ category }}</span
+              >
+            </div>
           </div>
-        </div>
 
-        <div class="flex">
-          <div class="facet-label">Technologies</div>
-          <div class="facet-list">
-            <span
-              class="resource-tag"
-              v-for="technology in resource.technologies"
-              :key="technology"
-              >{{ technology }}</span
-            >
+          <div class="flex">
+            <div class="facet-label">Technologies</div>
+            <div class="facet-list">
+              <span
+                class="resource-tag"
+                v-for="technology in resource.technologies"
+                :key="technology"
+                >{{ technology }}</span
+              >
+            </div>
           </div>
-        </div>
 
-        <div class="flex">
-          <div class="facet-label">Languages</div>
-          <div class="facet-list">
-            <span
-              class="resource-tag"
-              v-for="language in resource.languages"
-              :key="language"
-              >{{ language }}</span
-            >
+          <div class="flex">
+            <div class="facet-label">Languages</div>
+            <div class="facet-list">
+              <span
+                class="resource-tag"
+                v-for="language in resource.languages"
+                :key="language"
+                >{{ language }}</span
+              >
+            </div>
           </div>
         </div>
       </div>
--- ui/src/components/ResourceCardList.vue
+++ ui/src/components/ResourceCardList.vue
@@ -2,16 +2,9 @@
   <div class="resource-list">
     <div class="px-6 py-4">
       <div v-if="!resources">Loading...</div>
-      <div v-else v-for="resource in resources" :key="resource.RowKey">
-        <ResourceCard
-          v-if="/repository|extension/.test(resource.PartitionKey)"
-          :resource="resource"
-        />
-        <div class="mb-12" v-else>
-          <h3 class="text-2xl">Unknown Data Type</h3>
-          <p>Unknown partition key value: {{ resource.PartitionKey }}</p>
-        </div>
-      </div>
+      <template v-else v-for="resource in resources">
+        <ResourceCard :resource="resource" :key="resource.RowKey" />
+      </template>
     </div>
   </div>
 </template>
