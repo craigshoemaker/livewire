2a1cc2ddca1f222367df178ead5311d3862455b4
craigshoemaker <craig@craigshoemaker.net>
2020-10-27 15:53:09 -0400

    Add production base API url

--- ui/src/components/api.js
+++ ui/src/components/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: `api/`,
-});
+const isLocal = /localhost|127\.0\.0\.1/.test(window.location.host);
 
-const isLocalhost = /localhost|127\.0\.0\.1/.test(window.location.host);
+const axiosInstance = axios.create({
+  baseURL: isLocal ? 'api/' : `https://livewireapp.azurewebsites.net/api/`,
+});
 
 export const api = {
   async addResource(payload) {
-    const key = isLocalhost
+    const key = isLocal
       ? ''
       : '?code=JlpSRyXtLrtzLdcerBYJIEWLqbBuqRfaKV5sMDwgqT9UikvYvoOusQ==';
     return await axiosInstance.post(`./addResource${key}`, payload);
