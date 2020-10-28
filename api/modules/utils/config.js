5f5c94c8bbed1501ce31daf92a63ecfb617902cd
craigshoemaker <craig@craigshoemaker.net>
2020-10-28 14:55:55 -0400

    Remove explicit reading of local settings

--- api/modules/utils/config.js
+++ api/modules/utils/config.js
@@ -1,25 +1,5 @@
-const { readFileSync } = require("fs");
-const { join } = require("path");
-
-let storageConnectionString = "";
-let storageAccountName = "";
-let storageAccountKey;
-
-if (process.env.PRODUCTION) {
-  storageConnectionString = process.env.STORAGE_CONNECTION_STRING;
-  storageAccountName = process.env.STORAGE_ACCOUNT_NAME;
-  storageAccountKey = process.env.STORAGE_ACCOUNT_KEY;
-} else {
-  const filePath = join(__dirname, "../../local.settings.json");
-  const file = readFileSync(filePath, "utf8");
-  const localConfig = JSON.parse(file);
-  storageConnectionString = localConfig.Values.STORAGE_CONNECTION_STRING;
-  storageAccountName = localConfig.Values.STORAGE_ACCOUNT_NAME;
-  storageAccountKey = localConfig.Values.STORAGE_ACCOUNT_KEY;
-}
-
 module.exports = {
-  STORAGE_CONNECTION_STRING: storageConnectionString,
-  STORAGE_ACCOUNT_NAME: storageAccountName,
-  STORAGE_ACCOUNT_KEY: storageAccountKey,
+  STORAGE_CONNECTION_STRING: process.env.STORAGE_CONNECTION_STRING,
+  STORAGE_ACCOUNT_NAME: process.env.STORAGE_ACCOUNT_NAME,
+  STORAGE_ACCOUNT_KEY: process.env.STORAGE_ACCOUNT_KEY,
 };
